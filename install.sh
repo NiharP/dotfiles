6844c905c7dfcaec0e6e370d2504f960eb349de0
NiharPatil <nihar@NiharPatils-MacBook-Pro.local>
2024-04-16 05:02:56 -0700

    Fixed path in install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-ln -s ~/.dotfiles/oh-my-zsh ~/.oh-my-zsh
-ln -s ~/.dotfiles/vscode ~/.vscode
-ln -s ~/.dotfiles/bashrc ~/.bashrc
-ln -s ~/.dotfiles/fzf.bash ~/.fzf.bash
-ln -s ~/.dotfiles/fzf.zsh ~/.fzf.zsh
-ln -s ~/.dotfiles/vimrc ~/.vimrc
-ln -s ~/.dotfiles/zshrc ~/.zshrc
-ln -s ~/.dotfiles/zsh ~/.zsh
+# Get the current working directory where the script is run
+DOTFILES_DIR=$(pwd)
 
-
-
-
+# Create symbolic links using the current working directory path
+ln -s $DOTFILES_DIR/oh-my-zsh ~/.oh-my-zsh
+ln -s $DOTFILES_DIR/vscode ~/.vscode
+ln -s $DOTFILES_DIR/bashrc ~/.bashrc
+ln -s $DOTFILES_DIR/fzf.bash ~/.fzf.bash
+ln -s $DOTFILES_DIR/fzf.zsh ~/.fzf.zsh
+ln -s $DOTFILES_DIR/vimrc ~/.vimrc
+ln -s $DOTFILES_DIR/zshrc ~/.zshrc
+ln -s $DOTFILES_DIR/zsh ~/.zsh
